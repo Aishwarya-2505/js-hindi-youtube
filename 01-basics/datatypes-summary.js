d3c43c5907fcc460c2508c22b77843ac15ba203e
tony mode <aishwaryacherupally5@gmail.com>
2025-11-23 19:45:13 +0000

    change in location of files

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+// // primitive 
+const score =100
+const scoreValue =100.3
+const islogged=false
+const outsidetemp =null
+let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+const bignum =21345678876543n
+
+//reference and non primitive
+//arrays and objects and functions
+const heros =["df","fr","rt"]
+let myobj = {
+    name: "ish",
+    age: 12,
+}
+const myfunction =function() {
+    console.log("hlo ");
+    
+}
+// now console.log(typeof    do for all and see types );
